74f686267a70a5ffca671bc9d3d08ccf9a6ae030
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-06-16 11:25:00 +0000

    Third Commit

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ import Tooltip from "./Tooltip";
 const App = () => {
   return (
     <div>
-        <Tooltip text={'This is a tooltip'}>Hover over me</Tooltip>
         <Tooltip text={'This is another tooltip'}>Hover over me to see another tooltip</Tooltip>
     </div>
   )
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,9 +2,15 @@ import React from "react";
 
 export default function Tooltip({text, children}){
     return(
-        <div class="tooltip">
-            {children}
-            <span class="tooltiptext">{text}</span>
+        <>
+        <div class="tool-tip">
+            <h2 class="tooltip">Hover over me</h2>
+            <p><span class="tooltiptext">This is a tooltip</span></p>
         </div>
+        <div class="tool-tip">
+            <h2 class="tooltip">{children}</h2>
+            <p class="tooltip"><span class="tooltiptext">{text}</span></p>
+        </div>
+        </>
     )
 }
